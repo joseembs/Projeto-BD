815a8089bdfcb08b3f0a2f701a00ddf5cd9eebc2
Elis Rodrigues <elisrb2004@gmail.com>
2025-07-07 14:34:02 -0300

    atualiza scripts sql

---
+++ SQL/procedure.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE PROCEDURE matricular_alunos(codigo_turma VARCHAR)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	max_alunos INT;
+	atual_alunos INT;
+	capacidade_turma INT;
+BEGIN
+	SELECT Capacidade INTO max_alunos FROM Turma WHERE Codigo = codigo_turma;
+	SELECT COUNT(*) INTO atual_alunos FROM HistoricoFazParte WHERE fk_Turma_Codigo = codigo_turma;
+	capacidade_turma := max_alunos - atual_alunos;
+	
+	IF capacidade_turma > 0 THEN
+		WITH selecao AS (
+			SELECT Fila.fk_Turma_Codigo, Fila.fk_Aluno_Matricula FROM FilaSeMatricula Fila
+			WHERE Fila.fk_Turma_Codigo = codigo_turma ORDER BY Fila.Prioridade ASC LIMIT capacidade_turma
+		)
+		
+		INSERT INTO HistoricoFazParte(fk_Turma_Codigo, fk_Aluno_Matricula, Status)
+		SELECT fk_Turma_Codigo, fk_Aluno_Matricula, 'Matriculado' FROM selecao;
+
+		DELETE FROM FilaSeMatricula USING selecao
+		WHERE FilaSeMatricula.fk_Turma_Codigo = selecao.fk_Turma_Codigo
+		AND FilaSeMatricula.fk_Aluno_Matricula = selecao.fk_Aluno_Matricula;
+	END IF;	
+END;
+$$;
---
+++ SQL/view.sql
@@ -0,0 +1,8 @@
+CREATE VIEW Grade AS
+SELECT Aluno.Nome AS Nome, Aluno.Matricula AS Matricula, Turma.Codigo AS Turma, Disciplina.Nome AS Disciplina
+FROM
+	HistoricoFazParte
+	INNER JOIN Aluno ON HistoricoFazParte.fk_Aluno_Matricula = Aluno.Matricula
+	INNER JOIN Turma ON HistoricoFazParte.fk_Turma_Codigo = Turma.Codigo
+	INNER JOIN Disciplina ON Turma.fk_Disciplina_Codigo = Disciplina.Codigo
+WHERE HistoricoFazParte.Status = 'Matriculado';
